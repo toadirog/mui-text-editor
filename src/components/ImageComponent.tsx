a8ee514999937fb1698a6673917b3961bb7c58d0
toadirog <skobelevgeorge@gmail.com>
2023-12-11 22:24:33 +0400

    image clicked popover actions added

--- src/components/ImageComponent.tsx
+++ src/components/ImageComponent.tsx
@@ -1,6 +1,11 @@
-import type { LexicalEditor, NodeKey } from 'lexical';
-
-import { Suspense, useRef } from 'react';
+import { $getNodeByKey, type LexicalEditor, type NodeKey } from 'lexical';
+import Popover from '@mui/material/Popover';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import DownloadIcon from '@mui/icons-material/Download';
+import { MouseEvent, Suspense, useEffect, useRef, useState } from 'react';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { Divider } from '@mui/material';
 
 const imageCache = new Set();
 
@@ -17,9 +22,43 @@ function useSuspenseImage(src: string) {
   }
 }
 
+const downloadImage = async (src: string) => {
+  try {
+    const response = await fetch(src, { method: 'GET' });
+
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.statusText}`);
+    }
+
+    const buffer = await response.arrayBuffer();
+    const url = window.URL.createObjectURL(new Blob([buffer]));
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'image.png'); // You can customize the filename here
+    document.body.appendChild(link);
+    link.click();
+
+    // Clean up to avoid memory leaks
+    link.remove();
+    window.URL.revokeObjectURL(url);
+  } catch (err) {
+    console.error('Error downloading image:', err);
+  }
+};
+
+function removeNodeByKey(editor: LexicalEditor, nodeKey: string) {
+  editor.update(() => {
+    const node = $getNodeByKey(nodeKey);
+    if (node) {
+      node.remove();
+    }
+  });
+}
+
 function LazyImage({
   altText,
   className,
+  onClick,
   imageRef,
   src,
   width,
@@ -28,6 +67,7 @@ function LazyImage({
 }: {
   altText: string;
   className: string | null;
+  onClick: (event: MouseEvent<HTMLImageElement>) => void;
   height: 'inherit' | number;
   imageRef: { current: null | HTMLImageElement };
   maxWidth: number;
@@ -37,6 +77,7 @@ function LazyImage({
   useSuspenseImage(src);
   return (
     <img
+      onClick={onClick}
       className={className || undefined}
       src={src}
       alt={altText}
@@ -56,6 +97,7 @@ export default function ImageComponent({
   width,
   height,
   maxWidth,
+  nodeKey,
 }: {
   altText: string;
   caption: LexicalEditor;
@@ -68,23 +110,95 @@ export default function ImageComponent({
   width: 'inherit' | number;
   captionsEnabled: boolean;
 }): JSX.Element {
-  const imageRef = useRef<null | HTMLImageElement>(null);
+  const [editor] = useLexicalComposerContext();
+  const imageRef = useRef<HTMLImageElement | null>(null);
+  const [anchorEl, setAnchorEl] = useState<HTMLImageElement | null>(null);
+  const [triggerDownload, setTriggerDownload] = useState(false);
+
+  const handleDownloadImage = () => {
+    downloadImage(src);
+  };
+
+  const handleClick = (event: MouseEvent<HTMLImageElement>) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleRemoveImage = () => {
+    removeNodeByKey(editor, nodeKey);
+    handleClose();
+  };
+
+  useEffect(() => {
+    if (triggerDownload && imageRef.current) {
+      imageRef.current.click();
+      setTriggerDownload(false);
+    }
+  }, [triggerDownload]);
+
+  // const handleDownloadImage = () => {
+  //   const link = document.createElement('a');
+  //   link.href = src; // The src prop should be the URL of the image
+  //   link.download = 'downloadedImage'; // The default filename for the download
+  //   document.body.appendChild(link);
+  //   link.click();
+  //   document.body.removeChild(link);
+  // };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
 
   return (
     <Suspense fallback={null}>
-      <>
-        <div>
-          <LazyImage
-            className=""
-            src={src}
-            altText={altText}
-            imageRef={imageRef}
-            width={width}
-            height={height}
-            maxWidth={maxWidth}
-          />
-        </div>
-      </>
+      <div>
+        <LazyImage
+          className=""
+          onClick={handleClick}
+          src={src}
+          altText={altText}
+          imageRef={imageRef}
+          width={width}
+          height={height}
+          maxWidth={maxWidth}
+        />
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          transformOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+        >
+          <div style={{ display: 'flex' }}>
+            <IconButton
+              onClick={handleDownloadImage}
+              disableRipple
+              disableFocusRipple
+              aria-label="download"
+            >
+              <DownloadIcon />
+            </IconButton>
+            <Divider orientation="vertical" variant="middle" flexItem />
+            <IconButton
+              onClick={handleRemoveImage}
+              disableRipple
+              disableFocusRipple
+              aria-label="delete"
+            >
+              <DeleteIcon />
+            </IconButton>
+          </div>
+        </Popover>
+      </div>
     </Suspense>
   );
 }
